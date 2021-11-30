8cb4c2994d7feecea47be31d7edd353c8240b805
MaksBarabanshikov <MaksimBarabanshikov2001@gmail.com>
2021-11-30 12:51:04 +0400

    refactor itemsData

--- ethman-app/src/Pages/ReadComic/Magazine.js
+++ ethman-app/src/Pages/ReadComic/Magazine.js
@@ -3,22 +3,22 @@ import HTMLFlipBook from "react-pageflip";
 import "./Magazine.css"
 
 function Magazine({selectedPage}) {
-    return(
-        <HTMLFlipBook
-            width={600}
-            height={750}
-            size="stretch"
-            maxShadowOpacity={0.5}
-            showCover={true}
-            mobileScrollSupport={true}>
-            {selectedPage.map(selectedPage => {
-                return(
-                    <div className="page" key={selectedPage.pageNumber}>
-                        <img src={selectedPage.page} alt={selectedPage.pageNumber}/>
-                    </div>
-                )
-            })}
-        </HTMLFlipBook>
+    return (
+            <HTMLFlipBook
+                width={600}
+                height={750}
+                size="stretch"
+                maxShadowOpacity={0.5}
+                showCover={true}
+                mobileScrollSupport={true}>
+                {selectedPage.map(selectedPage => {
+                    return (
+                        <div className="page" key={selectedPage.pageNumber}>
+                            <img src={selectedPage.page} alt={selectedPage.pageNumber}/>
+                        </div>
+                    )
+                })}
+            </HTMLFlipBook>
 
     )
 }
--- ethman-app/src/assets/ItemsData.js
+++ ethman-app/src/assets/ItemsData.js
@@ -5,6 +5,7 @@ const itemsData = [
         name: "A Grim Cloud - Part 1",
         address: "A_Grim_Cloud_-_Part_1",
         img: "https://www.eth-men.com/images/catalogue/comics/1_A_Grim_Cloud_Sketch_small.png",
+        price: "300001 CLO",
         pages: [
             {
                 page: "https://www.eth-men.com/images/catalogue/comics/1_A_Grim_Cloud_Sketch/1.jpg",
@@ -62,6 +63,7 @@ const itemsData = [
         name: "A Grim Cloud - Part 1",
         address: "A_Grim_Cloud_Part_1_BW_Edition",
         img: "https://www.eth-men.com/images/catalogue/comics/1_A_Grim_Cloud_BW_small.png",
+        price: "300001 CLO",
         pages: [
             {
                 page: "https://www.eth-men.com/images/catalogue/comics/1_A_Grim_Cloud_BW/1.jpg",
@@ -118,150 +120,175 @@ const itemsData = [
         type: "figures",
         name: "Lord Zieg",
         img: "https://www.eth-men.com/images/catalogue/figures/1_Zieg.png",
+        price: "300001 CLO"
     },
     {
         "id": 4,
         type: "figures",
         name: "Godrick",
         img: "https://lh3.googleusercontent.com/2oXhRYTY6F7CRcewo2q4oFaLfkFwoOKAEbLYRE4ypItZRmvSAK0kevxYDEfyy-U4JoQo1CaZT85jNz2Et_-AdJx1=s0",
+        price: "300001 CLO"
     },
     {
         "id": 5,
         type: "figures",
         name: "Pranksy",
         img: "https://lh3.googleusercontent.com/0HxGT_4yPipyv-eYE2MzKdjltMhzTKvfgv7oIlmBqzCk8CP0Qd1GgE-ON8mP6Ju8rrYTuoQPPibjoKn1AO7d6KnJ=s0",
+        price: "300001 CLO"
     },
     {
         "id": 6,
         type: "figures",
         name: "CHET",
         img: "https://lh3.googleusercontent.com/kk74eIly2OuehoBlHcrxZempm_2KeeDlazdP5cWbKfZnlqt0Wf6eKPRAndI4FZiMUQqhqmMPWqeP0fNj_i9hW3EvPUnzymnvFyMe=s0",
+        price: "300001 CLO"
     },
     {
         "id": 7,
         type: "figures",
         name: "5412",
         img: "https://www.eth-men.com/images/catalogue/figures/5_5412.png",
+        price: "300001 CLO"
     },
     {
         "id": 8,
         type: "figures",
         name: "Nature",
         img: "https://www.eth-men.com/images/catalogue/figures/6_Nature.png",
+        price: "300001 CLO"
     },
     {
         "id": 9,
         type: "figures",
         name: "Ôtomo Keshin",
         img: "https://www.eth-men.com/images/catalogue/figures/7_Otomo.png",
+        price: "300001 CLO"
     },
     {
         "id": 10,
         type: "avatars",
         name: "#1",
         img: "https://www.eth-men.com/images/catalogue/avatars/1.png",
+        price: "300001 CLO"
     },
     {
         "id": 11,
         type: "avatars",
         name: "#2",
         img: "https://www.eth-men.com/images/catalogue/avatars/2.png",
+        price: "300001 CLO"
     },
     {
         "id": 12,
         type: "avatars",
         name: "#3",
         img: "https://www.eth-men.com/images/catalogue/avatars/3.png",
+        price: "300001 CLO"
     },
     {
         "id": 13,
         type: "avatars",
         name: "#4",
         img: "https://www.eth-men.com/images/catalogue/avatars/4.png",
+        price: "300001 CLO"
     },
     {
         "id": 14,
         type: "exclusive",
         name: "ETH-MEN Exclusive/1/The Showdown",
         img: "https://www.eth-men.com/images/catalogue/exclusive/2_eth-men_exclusives.png",
+        price: "300001 CLO"
     },
     {
         "id": 15,
         type: "exclusive",
         name: "Eth-Men Collectors Card #1 - Zieg - Common",
         img: "https://www.eth-men.com/images/catalogue/exclusive/3_eth-men_exclusives.gif",
+        price: "300001 CLO"
     },
     {
         "id": 16,
         type: "exclusive",
         name: "Eth-Men Collectors Card #2 - Zieg - Rare",
         img: "https://www.eth-men.com/images/catalogue/exclusive/4_eth-men_exclusives.gif",
+        price: "300001 CLO"
     },
     {
         "id": 17,
         type: "exclusive",
         name: "Eth-Men Collectors Card #5 - Murthre - Special Edition",
         img: "https://www.eth-men.com/images/catalogue/exclusive/17_eth-men_exclusives.gif",
+        price: "300001 CLO"
     },
     {
         "id": 18,
         type: "cryptovoxels",
         name: "Lord Zieg´s Ur Laserblaster",
         img: "https://www.eth-men.com/images/catalogue/cryptovoxels/lord_ziegs_ur_laserblaster.gif",
+        price: "300001 CLO"
     },
     {
         "id": 19,
         type: "cryptovoxels",
         name: "Godrick´s Hair and Mask",
         img: "https://www.eth-men.com/images/catalogue/cryptovoxels/godricks_hair_and_mask.gif",
+        price: "300001 CLO"
     },
     {
         "id": 20,
         type: "reavers",
         name: "Space Reavers - #1 - Gal Gebra",
         img: "https://www.eth-men.com/images/catalogue/reavers/1.jpg",
+        price: "300001 CLO"
     },
     {
         "id": 21,
         type: "reavers",
         name: "Space Reavers - #2 - Aldo Sec",
         img: "https://www.eth-men.com/images/catalogue/reavers/2.jpg",
+        price: "300001 CLO"
     },
     {
         "id": 22,
         type: "reavers",
         name: "Space Reavers - #3 - Dalgo Gosch",
         img: "https://www.eth-men.com/images/catalogue/reavers/3.gif",
+        price: "300001 CLO"
     },
     {
         "id": 23,
         type: "related",
         name: "#022 - Super (Crypto Dolphins x ETH-MEN)",
         img: "https://www.eth-men.com/images/catalogue/related/crypto_dolphins-eth-men.gif",
+        price: "300001 CLO"
     },
     {
         "id": 24,
         type: "3D",
         name: "Lord Zieg",
         img: "https://www.eth-men.com/images/catalogue/3d/1_zieg.png",
+        price: "300001 CLO"
     },
     {
         "id": 25,
         type: "3D",
         name: "Godrick",
         img: "https://www.eth-men.com/images/catalogue/3d/2_godrick.png",
+        price: "300001 CLO"
     },
     {
         "id": 26,
         type: "3D",
         name: "Pranksy",
         img: "https://www.eth-men.com/images/catalogue/3d/3_pranksy.png",
+        price: "300001 CLO"
     },
     {
         "id": 27,
         type: "3D",
         name: "CHET",
         img: "https://www.eth-men.com/images/catalogue/3d/4_chet.png",
+        price: "300001 CLO"
     }
 ]
 
